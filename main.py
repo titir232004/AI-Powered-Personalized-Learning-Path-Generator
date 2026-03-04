1c2fdb4dac337693517c26e64c491a85c5102959
titir232004 <162278370+titir232004@users.noreply.github.com>
2026-03-04 10:22:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -67,10 +67,7 @@ async def generate_path(request: GeneratePathRequest):
             "db_insert": "success" if save_response.data else "failed"
         }
     }
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
 
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 
 
