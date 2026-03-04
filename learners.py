9bb5e040c992f64cec91e752fd606eedaf0c8129
titir232004 <162278370+titir232004@users.noreply.github.com>
2026-03-04 16:33:37 +0530

    Update learners.py

--- learners.py
+++ learners.py
@@ -1,16 +1,18 @@
-from db import supabase
-
-
 def fetch_learner(user_id: str):
-
     response = supabase.table("learners") \
+        .select("*") \
+        .execute()
+
+    print("ALL ROWS:", response.data)
+
+    response_filtered = supabase.table("learners") \
         .select("*") \
         .eq("user_id", user_id) \
         .execute()
 
-    if not response.data:
+    print("FILTERED:", response_filtered.data)
+
+    if not response_filtered.data:
         return None
 
-
-    return response.data[0]
-
+    return response_filtered.data[0]
