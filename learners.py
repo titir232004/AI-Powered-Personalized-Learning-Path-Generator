2f17fa64b5c220de69bc638cb0838088a4287ffc
titir232004 <162278370+titir232004@users.noreply.github.com>
2026-03-04 15:40:15 +0530

    Update learners.py

--- learners.py
+++ learners.py
@@ -5,10 +5,11 @@ def fetch_learner(user_id: str):
 
     response = supabase.table("learners") \
         .select("*") \
-        .eq("user_id",user_id) \
+        .eq("id", user_id) \
         .execute()
 
     if not response.data:
         return None
+
 
     return response.data[0]
